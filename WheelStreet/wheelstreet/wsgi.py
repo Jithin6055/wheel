89a9c7a768e5ee6808b79b11a8ebf60764f368fe
Jithin6055 <jithinjoseph6055@gmail.com>
2024-09-30 16:32:45 +0530

    first commit

--- WheelStreet/wheelstreet/wsgi.py
+++ WheelStreet/wheelstreet/wsgi.py
@@ -10,6 +10,5 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-print("Current working directory:", os.getcwd())  # Debugging line
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wheelstreet.settings')
 application = get_wsgi_application()
